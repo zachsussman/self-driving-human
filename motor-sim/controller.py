beed35592bbb320c0c2f311a4de9556baef65c25
Zachary Sussman <zachary@zacharysussman.com>
2018-04-26 13:11:03 -0400

    redid line thingy

--- motor-sim/controller.py
+++ motor-sim/controller.py
@@ -8,13 +8,14 @@ try:
 except:
     pass
 
+def convert_polys(polys):
+    return [[(point['x'], point['y']) for point in poly] for poly in polygons]
 
-
-polygons = [(0, 0), (150, 100), (300, 300), (450, 100), (500, 200), (600, 0)]
+polygons = [[(0, 0), (0, 100), (100, 100), (100, 0)], [(200, 200), (200, 300), (300, 300), (300, 200)]]
 
 SCREENSIZE = 600
 
-COUNTS_PER_INCH = 1
+COUNTS_PER_INCH = 372.14285714285717
 
 def normalize(v):
     return v / np.linalg.norm(v)
@@ -115,22 +116,59 @@ def vector_to_line(A, B, P):
     return perp / np.linalg.norm(perp) * np.cross(A-B, A-P)/np.linalg.norm(B-A)
 
 
-def find_nearest(polygon, con):
+def segment_normal(l1, l2, ray=False):
+    (a, b) = l1
+    (c, d) = l2
+    normal = np.array(((b-a)[1], (a-b)[0]))
+    t = np.dot(normal, a-c) / np.dot(normal, d-c)
+    # print(l1, l2, t)
+    return 0 <= t and (ray or t <= 1)
+
+
+def sn(a, b, c, d, r=False):
+    return segment_normal((np.array(a), np.array(b)), (np.array(c), np.array(d)), ray=r)
+
+def segments_intersect(ray, line):
+    return segment_normal(ray, line) and segment_normal(line, ray, ray=True)
+
+def segments_for_polygon(polygon):
+    return [(np.array(polygon[i]), np.array(polygon[i+1])) for i in range(0, len(polygon) - 1)] + [(np.array(polygon[-1]), np.array(polygon[0]))]
+
+
+def polygon_intersect(polygon, point):
+    ray = (point, point + np.array((1, 0)))
+    segments = segments_for_polygon(polygon)
+    segment_intersections = [segments_intersect(ray, s) for s in segments]
+    count = 0
+    for b in segment_intersections: 
+        if b:
+            count += 1
+    return count % 2 != 0
+
+def find_nearest(segments, con):
     pos = con.position()
-    polygon2d = [np.array(p) for p in polygon]
     pos2d = np.array((pos[0], pos[2]))
-    vectors = []
-    for i in range(0, len(polygon2d) - 1):
-        vectors += [vector_to_line(polygon2d[i], polygon2d[i+1], pos2d)]
+    vectors = [vector_to_line(s[0], s[1], pos2d) for s in segments]
     m = min(vectors, key=lambda v: np.linalg.norm(v))
     return np.array((m[0], 0, m[1]))
 
+def find_correct_direction(polygons, con):
+    pos = con.position()
+    pos2d = np.array((pos[0], pos[2]))
+    polys = [p for p in polygons if polygon_intersect(p, pos2d)]
+    if len(polys) == 0:
+        return np.array((0, 0, 0))
+    else:
+        return find_nearest(segments_for_polygon(polys[0]), con)
 
 motor1 = Motor(np.array((0, 0, 0)), 1)
 motor2 = Motor(np.array((500, 20, 0)), 1)
 motor3 = Motor(np.array((450, 500, 0)), 2)
 con = Controller([motor1, motor2, motor3])
-con.set_position(np.array((300, 300, 100)))
+con.set_position(np.array((230, 300, 270)))
+
+print(find_correct_direction(polygons, con))
+# raise Exception()
 
 def test(position, velocity):
     con.set_position(position)
@@ -158,6 +196,10 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
 
         while ser and ser.in_waiting > 0:
             data = ser.read(5)
@@ -172,13 +214,18 @@ def main():
         con.set_velocity(np.array((0, 0, -1)))
         screen.fill((255, 255, 255))
 
-        vel = find_nearest(polygons, con)
+        vel = find_correct_direction(polygons, con)
         if vel[2] > 0: vel = np.array((0, 0, 0))
         vel = force_normalize(vel)
 
         con.set_velocity(vel * 0.5)
-        pygame.draw.lines(screen, (0, 128, 0), False, polygons)
+        pos2d = np.array((con.position()[0], con.position()[2]))
+        for poly in polygons:
+            color = (0, 128, 0) if polygon_intersect(poly, pos2d) else (0, 0, 128)
+            pygame.draw.polygon(screen, color, poly)
         con.draw(screen)
+
+        print(pos2d)
 
 
         pygame.display.flip()
