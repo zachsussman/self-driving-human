3881ae9db6e84590b5c623e358ce47d4f60a63fc
Zachary Sussman <zachary@zacharysussman.com>
2018-04-28 16:22:05 -0400

    updated to work with outlines, serial

--- main_pygame.py
+++ main_pygame.py
@@ -11,10 +11,13 @@ import serial
 
 ser = None
 try:
-    ser = serial.Serial('/dev/ttyUSB0')
+    ser = serial.Serial('/dev/tty.usbmodem00000001')
+    print("connected to serial")
 except:
     pass
 
+
+outline = [(-3000, 0), (-2000, 1000), (-1000, 1500), (-500, 2000), (0, 1000), (1000, 3000), (2000, 2000), (2500, 200), (3000, 0)]
 
 motor1 = Motor(np.array((-3000, 0, 0)), 1)
 motor2 = Motor(np.array((3000, 0, 0)), 1)
@@ -25,8 +28,12 @@ ld = Lidar("/dev/tty.SLAB_USBtoUART")
 lb = LidarBuffer(ld)
 lb.start()
 
+samples = lb.samples[:]
+print(samples)
 
-print("Hi")
+con.set_position(np.array((0, 200, 1000)))
+
+
 
 def end():
     pygame.quit()
@@ -47,19 +54,23 @@ def main():
         con.update_serial()
 
 
+
+
         mouse_pos = unconvert(pygame.mouse.get_pos())
         con.set_position(np.array((mouse_pos[0], 200, mouse_pos[1])))
         screen.fill((255, 255, 255))
+        con.set_outline(outline)
 
-        samples = lb.samples[:]
-        polygons = convert_polys(detect_polygons(samples), 0.05)
-        con.set_polygons(polygons)
-        con.set_correct_force()
+        # samples = lb.samples[:]
+        # polygons = convert_polys(detect_polygons(samples), 0.05)
+        # con.set_polygons(polygons)
+        con.move_from_outline()
 
-        for poly in polygons:
-            p = [convert2d(point) for point in poly]
-            color = (0, 128, 0) if polygon_intersect(p, mouse_pos) else (0, 0, 128)
-            pygame.draw.polygon(screen, color, p)
+        # for poly in polygons:
+        #     p = [convert2d(point) for point in poly]
+        #     color = (0, 128, 0) if polygon_intersect(p, mouse_pos) else (0, 0, 128)
+        #     pygame.draw.polygon(screen, color, p)
+        pygame.draw.lines(screen, (0, 128, 0), False, [convert2d(p) for p in outline])
         con.draw(screen)
 
         pygame.display.flip()
